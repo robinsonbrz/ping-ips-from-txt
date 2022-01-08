50a756e18ec1cfe59985108afb99d290de3a6ab2
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-01-07 23:16:46 -0300

    Files added
    Source code and ip_list added

---
+++ ping.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 22:51:29 2022
+
+@author: Robinson Enedino
+"""
+import time
+import os
+
+#pasta = ".\\" 
+
+
+
+def loopPing():
+    # Open ip_list.txt file
+    with  open( "ip_list.txt") as file:
+        dump = file.read()
+        dump = dump.splitlines()
+        #print(dump)
+    
+    
+    print("______________________________________________")
+    # Ping for each ip address
+    for ip in dump:
+        res = os.popen(f"ping {ip} ").read()
+        achouFalha = False
+        strFalhaPing = ["Falha", "Host", "Request"]
+        for palavra in strFalhaPing:
+            if palavra in res:
+                achouFalha = True
+        
+        if achouFalha:
+            horario = time.strftime("%H:%M %S-seg")
+            f = open( "down_output.txt", "a")
+            f.write(str(ip) + "\t is down " + horario  + "########" + "\n")
+            print(str(ip) + "\t is down "  + horario + "########")
+            f.close()
+        else:
+            horario = time.strftime("%H:%mm %S-seg")
+            print(str(ip) + "\t is up " + horario)
+
+while True:
+        loopPing()
+    
